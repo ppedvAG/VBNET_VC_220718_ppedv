e985308a56f82c56fdfee49f17951fbc9acf34e1
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-07-19 12:02:41 +0200

    OOP

---
+++ Fahrzeugpark/Fahrzeug.vb
@@ -0,0 +1,35 @@
+﻿Public Class Fahrzeug
+
+    Private _name As String
+    Public Property Name() As String
+        Get
+            Return _name
+        End Get
+        Set(ByVal value As String)
+            If value.Length > 0 Then
+                _name = value
+            End If
+        End Set
+    End Property
+
+
+    Private _maxGeschwindigkeit As Integer
+    Public Property MaxGeschwindigkeit() As Integer
+        Get
+            Return _maxGeschwindigkeit
+        End Get
+        Private Set(ByVal value As Integer)
+            _maxGeschwindigkeit = value
+        End Set
+    End Property
+
+    Function BeschreibeMich() As String
+        Return $"{Name} kann maximal {MaxGeschwindigkeit} km/h fahren."
+    End Function
+
+    Sub New(name As String, maxG As Integer)
+        _name = name
+        _maxGeschwindigkeit = maxG
+    End Sub
+
+End Class
---
+++ TesteFahrzeugpark/Module1.vb
@@ -0,0 +1,27 @@
+﻿Imports Fahrzeugpark
+
+Module Module1
+
+    Sub Main()
+
+        Dim fz1 As Fahrzeugpark.Fahrzeug = New Fahrzeug("BMW", 230)
+        Dim fz2 As Fahrzeug = New Fahrzeug("Mercedes", 340)
+
+        Console.WriteLine(fz1.Name)
+
+        fz1.Name = "Audi"
+        Console.WriteLine(fz1.Name)
+
+        fz1.Name = ""
+        Console.WriteLine(fz1.Name)
+
+        Console.WriteLine(fz1.BeschreibeMich())
+        Console.WriteLine(fz2.BeschreibeMich())
+
+        Console.WriteLine(fz1.MaxGeschwindigkeit)
+        'fz1.MaxGeschwindigkeit = 123
+
+        Console.ReadKey()
+    End Sub
+
+End Module
