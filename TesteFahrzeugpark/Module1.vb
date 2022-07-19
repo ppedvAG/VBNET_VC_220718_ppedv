01520254d2de05f57cc5238237aa5403e6a3c9e6
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-07-19 12:05:38 +0200

    OOP (kommentiert)

--- TesteFahrzeugpark/Module1.vb
+++ TesteFahrzeugpark/Module1.vb
@@ -3,23 +3,47 @@
 Module Module1
 
     Sub Main()
+#Region "Modul 05 OOP"
 
-        Dim fz1 As Fahrzeugpark.Fahrzeug = New Fahrzeug("BMW", 230)
-        Dim fz2 As Fahrzeug = New Fahrzeug("Mercedes", 340)
+        'Dim fz1 As Fahrzeug = New Fahrzeug("BMW", 240)
+        'Dim fz2 As Fahrzeug = New Fahrzeug("Mercedes", 250)
 
+        'Console.WriteLine(fz1.Name)
+
+        'fz1.Name = "Audi"
+        'Console.WriteLine(fz1.Name)
+
+        'Console.WriteLine(fz2.Name)
+
+        'Console.WriteLine(fz1.BeschreibeMich())
+
+#End Region
+
+#Region "Lab 05 Fahrzeug-Klasse"
+
+        'Deklaration der Fahrzeugvariablen und Initialisierung der Fahrzeugobjekte
+        Dim fz1 As Fahrzeug = New Fahrzeug("BMW", 270, 25000)
+        Dim fz2 As Fahrzeug = New Fahrzeug("Mercedes", 260, 28000)
+
+        'Ausgabe der Name-Property von fz1 (über deren Getter)
         Console.WriteLine(fz1.Name)
 
-        fz1.Name = "Audi"
-        Console.WriteLine(fz1.Name)
+        'Ausführen von Methoden von fz1
+        fz1.StarteMotor()
+        fz1.Beschleunige(80)
 
-        fz1.Name = ""
-        Console.WriteLine(fz1.Name)
+        'Ausgabe der AktGeschwindigkeit-Properties
+        Console.WriteLine(fz1.AktGeschwindigkeit)
+        Console.WriteLine(fz2.AktGeschwindigkeit)
 
+        'Ausführen einer Methode von fz1
+        fz1.Beschleunige(300)
+
+        'Ausgabe der BeschreibeMich()-Methoden
         Console.WriteLine(fz1.BeschreibeMich())
         Console.WriteLine(fz2.BeschreibeMich())
 
-        Console.WriteLine(fz1.MaxGeschwindigkeit)
-        'fz1.MaxGeschwindigkeit = 123
+#End Region
 
         Console.ReadKey()
     End Sub
